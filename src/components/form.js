2c810e5b5c0df5a55b98c14d38ce9eebf5d44810
Drunk-dad <gashek1994@mail.ru>
2021-04-27 15:40:24 +0300

    add push button in form

--- src/components/form.js
+++ src/components/form.js
@@ -1,15 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
+import { Button } from "./button";
 import { InputFile } from "./inputFile";
 import { InputText } from "./inputText";
 
 export const Form = (props) => {
-  
+
 	
 	return (
     <form className="form_twitt">
 			<InputText/>
       <div className="input_file_wrp">
 				<InputFile/>
+				<Button/>
 			</div>
     </form>
   );
